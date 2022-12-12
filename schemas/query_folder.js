1e5d388f38bf1b6da72a32116de7107f47fd33df
yuldashboev <team19devs@gmail.com>
2022-12-12 16:42:02 +0500

    add left models to schemas

---
+++ schemas/query_folder.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+const { v4 } = require("uuid")
+
+const QueryFolderSchema = mongoose.Schema(
+    {
+        id: {
+            type: String,
+            default: v4,
+            unique: true
+        },
+
+        title: {
+            type: String,
+            required: [true, "Query Folder must have a title"],
+        },
+        parent_id: {
+            type: String,
+        }
+    },
+    {
+        timestamps: {createdAt: "created_at", updatedAt: "updated_at"},
+        toObject: {virtuals: true},
+        toJSON: {virtuals: true},
+    }
+);
+
+module.exports =  QueryFolderSchema

e4b8c62827c05d1f07b69e770f806c1bb56b5c8b
yuldashboev <team19devs@gmail.com>
2022-12-14 11:15:24 +0500

    fix mongo conn

--- config/mongoConn.js
+++ config/mongoConn.js
@@ -7,15 +7,15 @@ async function newMongoConn(Config) {
     let mongoDBUrl = 
     //`mongodb://medion_node_object_builder_service:Weipheingo7aeCho@46.101.114.171:27017/medion_node_object_builder_service`
     "mongodb://" +
-    cfg.mongoUser +
+    Config.mongoUser +
     ":" +
-    cfg.mongoPassword +
+    Config.mongoPassword +
     "@" +
-    cfg.mongoHost +
+    Config.mongoHost +
     ":" +
-    cfg.mongoPort +
+    Config.mongoPort +
     "/" +
-    cfg.mongoDatabase;
+    Config.mongoDatabase;
 
     let options = {
         // poolSize: 10,

9acd8d4ab8d152a18ffa7c206fe8ab2bd72b335c
Sirojiddin-kh <sirojiddinhamodov2001@gmail.com>
2022-12-26 12:29:03 +0500

    added transasia project creds for client

--- index.js
+++ index.js
@@ -19,18 +19,18 @@ const service = require('./services/project');
     })
    
 
-    // This for checking in localhost
+  //  This for checking in localhost
 
-    // await projectStorage.registerProjects({
-    //     project_id: "test12123",
-    //     credentials: {
-    //         host: "localhost",
-    //         port: 27017,
-    //         database: "new_test",
-    //         username: "sirojiddin",
-    //         password: "1234",
-    //     }
-    // })
+    await projectStorage.registerProjects({
+        project_id: "50008d64-d3ce-47d0-98b5-cfce24cc7be0",
+        credentials: {
+            host: "161.35.26.178",
+            port: 27017,
+            database: "transasia_transasia_object_builder_service",
+            username: "transasia_transasia_object_builder_service",
+            password: "123JFWxq",
+        }
+    })
     
 
     const mongooseConnection = require("./config/mongooseConnection");

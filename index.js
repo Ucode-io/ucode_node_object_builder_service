819207af7292b58aa7f42bae2e8b13ee46b6c68b
Sirojiddin-kh <sirojiddinhamodov2001@gmail.com>
2022-12-29 21:37:56 +0500

    static project is removed

--- index.js
+++ index.js
@@ -9,16 +9,16 @@ const { struct } = require('pb-util');
 
 (async function () {
 
-    await projectStorage.reconnect({
-        project_id: '39f1b0cc-8dc3-42df-b2bf-813310c007a4',
-        credentials: {
-            host: '161.35.26.178',
-            port: 27017,
-            database: 'transasia_transasia_object_builder_service',
-            username: 'transasia_transasia_object_builder_service',
-            password: '123JFWxq'
-        },
-    })
+    // await projectStorage.reconnect({
+    //     project_id: '39f1b0cc-8dc3-42df-b2bf-813310c007a4',
+    //     credentials: {
+    //         host: '161.35.26.178',
+    //         port: 27017,
+    //         database: 'transasia_transasia_object_builder_service',
+    //         username: 'transasia_transasia_object_builder_service',
+    //         password: '123JFWxq'
+    //     },
+    // })
 
 
 

b31e6ac348eee75a5552cd63cc165a56c8dee623
yuldashboev <team19devs@gmail.com>
2022-12-29 17:58:05 +0500

    fixed user migration fields

--- initial_setups/user.js
+++ initial_setups/user.js
@@ -1,17 +1,8 @@
 
 async function createUser(userID, roleID, clientTypeID, clientPlatformID, projectID) {
     let userData = [{
-        "phone": "(90) 150-10-69",
-        "work_place": "<p>Udevs</p>",
         "active": 1,
-        "passport_number": "111111",
         "expires_at": "2026-08-21T09:03:03.905Z",
-        "name": "Admin",
-        "photo_url": "https://cdn.medion.uz/medion/d9bd2cd6-ada1-452a-849f-32fbfa81dd1d_Medion.png",
-        "verified": true,
-        "email": "jeesbek@gmail.com",
-        "passpost_serial": "AA",
-        "salary": "2000000",
         "guid": userID,
         "role_id": roleID,
         "client_type_id": clientTypeID,
@@ -20,7 +11,6 @@ async function createUser(userID, roleID, clientTypeID, clientPlatformID, projec
         "createdAt": new Date(),
         "updatedAt": new Date(),
         "__v": 0,
-        "surname": ""
       }]
 
       return userData

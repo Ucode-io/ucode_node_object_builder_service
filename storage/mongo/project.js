ea4fcde658cdb868a3bd7fd205a190a3e4ae599f
euler <elyorbekuralov@gmail.com>
2023-01-05 19:12:45 +0500

    resource id added

--- storage/mongo/project.js
+++ storage/mongo/project.js
@@ -23,6 +23,10 @@ let projectStore = {
                 throw new Error('Error project_id is required')
             }
 
+            if (!data.resource_id) {
+                throw new Error('Error resource_id is required')
+            }
+
             const mongoDBConn = await newMongoDBConn({
                 mongoHost: data.credentials.host,
                 mongoPort: data.credentials.port,
@@ -33,7 +37,7 @@ let projectStore = {
 
             await insertCollections(mongoDBConn, data.user_id, data.project_id)
 
-            await pool.add(data.project_id, mongoDBConn)
+            await pool.add(data.resource_id, mongoDBConn)
 
             mongoDBConn.once("open", async function () {
                 console.log("Connected to the database, building models");

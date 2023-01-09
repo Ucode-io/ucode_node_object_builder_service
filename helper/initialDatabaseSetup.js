862c8345cd118adc17077c5ec16967e0cbb937a5
yuldashboev <team19devs@gmail.com>
2023-01-09 12:00:00 +0500

    resource register: check collection exists before running migration

--- helper/initialDatabaseSetup.js
+++ helper/initialDatabaseSetup.js
@@ -17,98 +17,149 @@ const createRelation = require("../initial_setups/relation");
 
 async function insertCollections(conn, userId, projectId) {
 
-        const projectID = projectId.toString()
-        const clientPlatformID = v4().toString()
-        const clientTypeID = v4().toString()
-        const roleID = v4().toString()
-        const userID = userId.toString()
-        const testLoginID = v4().toString()
-        const connectionID = v4().toString()
+    const projectID = projectId.toString()
+    const clientPlatformID = v4().toString()
+    const clientTypeID = v4().toString()
+    const roleID = v4().toString()
+    const userID = userId.toString()
+    const testLoginID = v4().toString()
+    const connectionID = v4().toString()
 
+    
+    collections = await new Promise((resolve, reject) => {
+        let collections = {}
+        conn.db.listCollections()
+            .toArray(function (err, collectionNames) {
+                if (err) {
+                    console.log(err);
+                    reject(err)
+                }
+
+                for (let collection of collectionNames) {
+                    collections[collection.name] = collection.name
+                }
+
+                resolve(collections)
+            });
+    })
+
+    console.log('available collections', collections)
+
+
+    if (!collections['apps']) {
         const app = await appCreate()
-
-        conn.collection('apps').insertMany(app, function (err, result) {
+        await conn.collection('apps').insertMany(app, function (err, result) {
             if (err) throw err;
             console.log("Inserted Apps : ", result.insertedCount)
         })
+    }
 
-        const clientPlatform = await createClientPlatform(clientPlatformID, clientTypeID, projectID)
-        conn.collection('client_platforms').insertMany(clientPlatform, function (err, result) {
-            if (err) throw err;
-            console.log("Inserted Client Platform : ", result.insertedCount)
-        })
-
-        const clientType = await createClientType(clientPlatformID, clientTypeID, projectID)
-        conn.collection('client_types').insertMany(clientType, function (err, result) {
-            if (err) throw err;
-            console.log("Inserted Client Type :", result.insertedCount)
-        })
-
-        const role = await createRole(roleID, clientPlatformID, clientTypeID, projectID)
-        conn.collection('roles').insertMany(role, function (err, result) {
-            if (err) throw err;
-            console.log("Inserted Role :", result.insertedCount)
-        })
-
-        const user = await createUser(userID, roleID, clientTypeID, clientPlatformID, projectID)
-        conn.collection('users').insertMany(user, function (err, result) {
-            if (err) throw err;
-            console.log("Inserted User :", result.insertedCount)
-        })
-
-        const testLogin = await createTestLogin(testLoginID, clientTypeID)
-        conn.collection('test_logins').insertMany(testLogin, function (err, result) {
-            if (err) throw err;
-            console.log("Inserted Test Login :", result.insertedCount)
-        })
-
-        const connections = await createConnection(connectionID, clientTypeID)
-        conn.collection('connections').insertMany(connections, function (err, result) {
-            if (err) throw err;
-            console.log("Inserted Connections : ", result.insertedCount)
-        })
-
+    if (!collections['tables']) {
         const table = await createTable()
         conn.collection('tables').insertMany(table, function (err, result) {
             if (err) throw err;
             console.log("Inserted Table :", result.insertedCount)
         })
+    }
 
-        const relation = await createRelation()
-        conn.collection('relations').insertMany(relation, function (err, result) {
-            if (err) throw err;
-            console.log("Inserted Relation :", result.insertedCount)
-        })
-
-        const recordPermission = await createRecordPermision(roleID)
-        conn.collection('record_permissions').insertMany(recordPermission, function (err, result) {
-            if (err) throw err;
-            console.log("Inserted Record Permission :", result.insertedCount)
-        })
-
+    if (!collections['fields']) {
         const fields = await createField()
         conn.collection('fields').insertMany(fields, function (err, result) {
             if (err) throw err;
             console.log("Inserted Field :", result.insertedCount)
         })
 
-        const fieldPermissions = await createFieldPermission(roleID)
-        conn.collection('field_permissions').insertMany(fieldPermissions, function (err, result) {
-            if (err) throw err;
-            console.log("Inserted Field Permissions :", result.insertedCount)
-        })
+    }
 
+    if (!collections['sections']) {
         const section = await createSection()
         conn.collection('sections').insertMany(section, function (err, result) {
             if (err) throw err;
             console.log("Inserted Section :", result.insertedCount)
         })
+    }
+
+
+    if (!collections['client_platforms']) {
+        const clientPlatform = await createClientPlatform(clientPlatformID, clientTypeID, projectID)
+        conn.collection('client_platforms').insertMany(clientPlatform, function (err, result) {
+            if (err) throw err;
+            console.log("Inserted Client Platform : ", result.insertedCount)
+        })
+    }
+
+    if (!collections['client_types']) {
+        const clientType = await createClientType(clientPlatformID, clientTypeID, projectID)
+        conn.collection('client_types').insertMany(clientType, function (err, result) {
+            if (err) throw err;
+            console.log("Inserted Client Type :", result.insertedCount)
+        })
+    }
+
+    if (!collections['roles']) {
+        const role = await createRole(roleID, clientPlatformID, clientTypeID, projectID)
+        conn.collection('roles').insertMany(role, function (err, result) {
+            if (err) throw err;
+            console.log("Inserted Role :", result.insertedCount)
+        })
+    }
+
+    if (!collections['users']) {
+        const user = await createUser(userID, roleID, clientTypeID, clientPlatformID, projectID)
+        conn.collection('users').insertMany(user, function (err, result) {
+            if (err) throw err;
+            console.log("Inserted User :", result.insertedCount)
+        })
+    }
+
+    if (!collections['test_logins']) {
+        const testLogin = await createTestLogin(testLoginID, clientTypeID)
+        conn.collection('test_logins').insertMany(testLogin, function (err, result) {
+            if (err) throw err;
+            console.log("Inserted Test Login :", result.insertedCount)
+        })
+    }
+
+    if (!collections['connections']) {
+        const connections = await createConnection(connectionID, clientTypeID)
+        conn.collection('connections').insertMany(connections, function (err, result) {
+            if (err) throw err;
+            console.log("Inserted Connections : ", result.insertedCount)
+        })
+    }
+
+    if (!collections['relations']) {
+        const relation = await createRelation()
+        conn.collection('relations').insertMany(relation, function (err, result) {
+            if (err) throw err;
+            console.log("Inserted Relation :", result.insertedCount)
+        })
+    }
+
+    if (!collections['record_permissions']) {
+        const recordPermission = await createRecordPermision(roleID)
+        conn.collection('record_permissions').insertMany(recordPermission, function (err, result) {
+            if (err) throw err;
+            console.log("Inserted Record Permission :", result.insertedCount)
+        })
+    }
+
+    if (!collections['field_permissions']) {
+        const fieldPermissions = await createFieldPermission(roleID)
+        conn.collection('field_permissions').insertMany(fieldPermissions, function (err, result) {
+            if (err) throw err;
+            console.log("Inserted Field Permissions :", result.insertedCount)
+        })
+    }
+
+    if (!collections['view_permissions']) {
 
         const viewRelationPermissions = await createViewRelationPermissions(roleID)
         conn.collection('view_permissions').insertMany(viewRelationPermissions, function (err, result) {
             if (err) throw err;
             console.log("Inserted View Permissions :", result.insertedCount)
         })
+    }
 
 }
 
--- storage/mongo/project.js
+++ storage/mongo/project.js
@@ -1,4 +1,4 @@
-    const pool = require("../../pkg/pool")
+const pool = require("../../pkg/pool")
 const catchWrapDb = require("../../helper/catchWrapDb");
 const insertCollections = require("../../helper/initialDatabaseSetup");
 const newMongoDBConn = require('../../config/mongoConn')
@@ -13,49 +13,68 @@ let NAMESPACE = "storage.project";
 
 let projectStore = {
     register: catchWrapDb(`${NAMESPACE}.register`, async (data) => {
-        try {
-
-            console.log('data-->',data)
-            if (!data.user_id) {
-                throw new Error('Error user_id is required')
-            }
-            if (!data.project_id) {
-                throw new Error('Error project_id is required')
-            }
-
-            const mongoDBConn = await newMongoDBConn({
-                mongoHost: data.credentials.host,
-                mongoPort: data.credentials.port,
-                mongoDatabase: data.credentials.database,
-                mongoUser: data.credentials.username,
-                mongoPassword: data.credentials.password
-            })
-
-            await insertCollections(mongoDBConn, data.user_id, data.project_id)
-
-            await pool.add(data.project_id, mongoDBConn)
-
-            mongoDBConn.once("open", async function () {
-                console.log("Connected to the database, building models");
-                await objectBuilder(false, data.project_id)
-                console.log("Object builder has successfully runned for", data.project_id);
-            });
-
-            return {}
-
-        } catch (err) {
-            throw err
+        if (!data.user_id) {
+            throw new Error('Error user_id is required')
         }
+        if (!data.project_id) {
+            throw new Error('Error project_id is required')
+        }
+
+        if (!data.resource_id) {
+            throw new Error('Error resource_id is required')
+        }
+
+        // shouldCompileModels=false since we have to list 
+        // existing collections in mongodb before running migrations
+        const mongoDBConn = await newMongoDBConn({
+            mongoHost: data.credentials.host,
+            mongoPort: data.credentials.port,
+            mongoDatabase: data.credentials.database,
+            mongoUser: data.credentials.username,
+            mongoPassword: data.credentials.password
+        }, false)
+
+        await insertCollections(mongoDBConn, data.user_id, data.project_id)
+
+        // compiling models after running migrations
+        mongoDBConn.model('App', require('../schemas/app'))
+        mongoDBConn.model('CustomEvent', require('../schemas/custom_event'))
+        mongoDBConn.model('Dashboard', require('../schemas/dashboard'))
+        mongoDBConn.model('Document', require('../schemas/document'))
+        mongoDBConn.model('EventLog', require('../schemas/event_log'))
+        mongoDBConn.model('Event', require('../schemas/event'))
+        mongoDBConn.model('Field', require('../schemas/field'))
+        mongoDBConn.model('Function', require('../schemas/function'))
+        mongoDBConn.model('HtmlTemplate', require('../schemas/html_template'))
+        mongoDBConn.model('Panel', require('../schemas/panel'))
+        mongoDBConn.model('QueryFolder', require('../schemas/query_folder'))
+        mongoDBConn.model('Query', require('../schemas/query'))
+        mongoDBConn.model('Relation', require('../schemas/relation'))
+        mongoDBConn.model('Section', require('../schemas/section'))
+        mongoDBConn.model('Table', require('../schemas/table'))
+        mongoDBConn.model('Variable', require('../schemas/variable'))
+        mongoDBConn.model('ViewRelation', require('../schemas/view_relation'))
+        mongoDBConn.model('View', require('../schemas/view'))
+        mongoDBConn.model('WebPage', require('../schemas/web_pages'))
+
+        await pool.add(data.resource_id, mongoDBConn)
+
+        mongoDBConn.once("open", async function () {
+            console.log("Connected to the database, building models");
+
+            await objectBuilder(false, data.resource_id)
+
+            console.log("Object builder has successfully runned for", data.resource_id);
+        });
+
+        return {}
     }),
 
     deregister: catchWrapDb(`${NAMESPACE}.deregister`, async (data) => {
-        try {
-            await pool.remove(data?.project_id)
-            return {}
 
-        } catch (err) {
-            throw err
-        }
+        await pool.remove(data?.project_id)
+        return {}
+
     }),
 
     registerMany: catchWrapDb(`${NAMESPACE}.registerMany`, async (data) => {
@@ -67,100 +86,78 @@ let projectStore = {
     }),
 
     getByID: catchWrapDb(`${NAMESPACE}.getByID`, async (data) => {
-        try {
-            const mongoConn = await pool.get(data?.project_id)
-            return mongoConn
 
-        } catch (err) {
-            throw err
-        }
+        const mongoConn = await pool.get(data?.project_id)
+        return mongoConn
+
     }),
 
     reconnect: catchWrapDb(`${NAMESPACE}.reconnect`, async (data) => {
-        try {
-            const mongoDBConn = await newMongoDBConn({
-                mongoHost: data.credentials.host,
-                mongoPort: data.credentials.port,
-                mongoDatabase: data.credentials.database,
-                mongoUser: data.credentials.username,
-                mongoPassword: data.credentials.password
-            })
 
-            await pool.override(data?.project_id, mongoDBConn)
+        const mongoDBConn = await newMongoDBConn({
+            mongoHost: data.credentials.host,
+            mongoPort: data.credentials.port,
+            mongoDatabase: data.credentials.database,
+            mongoUser: data.credentials.username,
+            mongoPassword: data.credentials.password
+        })
 
+        await pool.override(data?.project_id, mongoDBConn)
 
-            await new Promise( (resolve, reject) => {
-                try {
-                    mongoDBConn.once("open", async function () {
-                        console.log("Connected to the database, building models for", data.project_id);
-                        await objectBuilder(false, data.project_id)
-                        console.log("Object builder has successfully runned for", data.project_id);
-                        resolve()
-                    });
-                } catch (err) {
-                    reject(err)
-                }
-               
-            })
+        await new Promise((resolve, reject) => {
+            try {
+                mongoDBConn.once("open", async function () {
+                    console.log("Connected to the database, building models for", data.project_id);
+                    await objectBuilder(false, data.project_id)
+                    console.log("Object builder has successfully runned for", data.project_id);
+                    resolve()
+                });
 
-            return {}
+            } catch (err) {
+                reject(err)
+            }
 
-        } catch (err) {
-            throw err
-        }
+        })
+
+        return {}
     }),
 
     registerProjects: catchWrapDb(`${NAMESPACE}.register`, async (data) => {
-        try {
-            const mongoDBConn = await newMongoDBConn({
-                mongoHost: data.credentials.host,
-                mongoPort: data.credentials.port,
-                mongoDatabase: data.credentials.database,
-                mongoUser: data.credentials.username,
-                mongoPassword: data.credentials.password
+
+        // shouldCompileModels=false since we have to list 
+        // existing collections in mongodb before running migrations
+        const mongoDBConn = await newMongoDBConn({
+            mongoHost: data.credentials.host,
+            mongoPort: data.credentials.port,
+            mongoDatabase: data.credentials.database,
+            mongoUser: data.credentials.username,
+            mongoPassword: data.credentials.password
+        }, false)
+
+        await insertCollections(mongoDBConn, data.user_id, data.project_id)
+
+        await pool.add(data?.project_id, mongoDBConn)
+
+        mongoDBConn.once("open", async function () {
+            console.log("Connected to the database, building models");
+
+            await objectBuilder(false, data?.project_id).then(res => {
+                console.log("Object builder has successfully runned for", data?.project_id);
             })
+        });
 
-            await insertCollections(mongoDBConn, data.user_id, data.project_id)
+        return {}
 
-            await pool.add(data?.project_id, mongoDBConn)
-
-            mongoDBConn.once("open", async function () {
-                console.log("Connected to the database, building models");
-
-                await objectBuilder(false, data?.project_id).then(res => {
-                    console.log("Object builder has successfully runned for", data?.project_id);
-                })
-            });
-
-            return {}
-
-        } catch (err) {
-            throw err
-        }
     }),
     autoConnect: catchWrapDb(`${NAMESPACE}.autoConnect`, async (args) => {
+
         if (!config.k8s_namespace) { throw new Error("k8s_namespace is required to get project") };
-        console.log("args ==> ", args)
+
         let projects = await client.autoConn(config.k8s_namespace)
-        console.log('projects', projects)
+
         return projects;
     })
 };
-
-
-// async function AutoConn(args){
-
-//     if (!config.k8s_namespace) { throw new Error("k8s_spaceName is REQUIRED") };
-//     let query = {
-//         // query
-//         _id: config.k8s_namespace
-//     }
-
-//     let resConn = await client.ProjectService.AutoConnect(query);
-
-//     return resConn
-// }
-// module.exports = {AutoConn};
 
 module.exports = projectStore;
 

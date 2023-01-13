a54afb14e5088f1a07ca6e4a52d0e97c81d018cb
euler <elyorbekuralov@gmail.com>
2023-01-13 18:24:48 +0500

    Revert "check log"
    This reverts commit 2d67a7aa057260440892827fb0ed7914a497dd6d.

--- pkg/pool.js
+++ pkg/pool.js
@@ -1,5 +1,5 @@
 const config = require('../config/index');
-let pool = {};
+let pool = new Map();
 
 // const interval = setInterval(() => {
     // for (projectID of pool.keys()) {
@@ -19,12 +19,11 @@ async function get(projectId) {
         console.warn('WARNING:: Using default project id in pool...')
     }
 
-    // if (!pool.has(projectId)) {
-    //     throw ErrProjectNotExists
-    // }
+    if (!pool.has(projectId)) {
+        throw ErrProjectNotExists
+    }
 
-    // conn = pool.get(projectId)
-    conn = pool[projectId]
+    conn = pool.get(projectId)
     console.log('POOL GET METHOD PROJECTID', projectId)
     console.log('POOL GET METHOD CONNECTION', conn)
     return conn
@@ -35,12 +34,11 @@ async function add(projectId, dbConn) {
         console.warn('WARNING:: Using default project id in pool...')
     }
 
-    // if (pool.has(projectId)) {
-    //     throw ErrProjectExists
-    // }
+    if (pool.has(projectId)) {
+        throw ErrProjectExists
+    }
 
-    // pool.set(projectId, dbConn)
-    pool[projectId] = dbConn
+    pool.set(projectId, dbConn)
 }
 
 async function remove(projectId) {
@@ -48,9 +46,7 @@ async function remove(projectId) {
         console.warn('WARNING:: Using default project id in pool...')
     }
 
-    // pool.delete(projectId)
-
-    delete pool[projectId]
+    pool.delete(projectId)
 }
 
 async function override(projectId, dbConn) {
@@ -58,11 +54,11 @@ async function override(projectId, dbConn) {
         console.warn('WARNING:: Using default project id in pool...')
     }
 
-    // if (pool.has(projectId)) {
-    //     console.warn('WARNING:: Overriding project in pool...', projectId)
-    // }
+    if (pool.has(projectId)) {
+        console.warn('WARNING:: Overriding project in pool...', projectId)
+    }
 
-    pool[projectId] = dbConn
+    pool.set(projectId, dbConn)
 }
 
 module.exports = {get, add, remove, override}

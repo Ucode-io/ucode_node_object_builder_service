2351bb7908061058488b6b83ea4fd24486968821
yuldashboev <team19devs@gmail.com>
2023-01-27 18:03:26 +0500

    temp rm commit_id, commit_guid

--- schemas/section.js
+++ schemas/section.js
@@ -36,14 +36,14 @@ const SectionSchema = mongoose.Schema(
             type: Boolean,
             default: false
         },
-        commit_id: {
-            type: Number,
-            required: [true, "commit_id is required"],
-        },
-        commit_guid: {
-            type: String,
-            required: [true, "commit_guid is required"],
-        }
+        // commit_id: {
+        //     type: Number,
+        //     required: [true, "commit_id is required"],
+        // },
+        // commit_guid: {
+        //     type: String,
+        //     required: [true, "commit_guid is required"],
+        // }
     },
     {
         timestamps: { createdAt: "created_at", updatedAt: "updated_at" },

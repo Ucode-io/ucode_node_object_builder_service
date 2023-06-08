d20c958fd521e33f5c625aa41f413725f3a5b6aa
valiyev_muhammadjon <valiyevmuhammadjon@gmail.com>
2023-06-08 19:24:00 +0500

    fix default role

--- helper/defaultRole.js
+++ helper/defaultRole.js
@@ -13,29 +13,29 @@ module.exports = async (mongoConn, projectId) => {
         const FieldPermission = mongoConn.models["field_permission"]
 
         // check guess client type
-        let guessClientType = await ClientType.findOne({guid: inittialClientType.guid})
+        let guessClientType = await ClientType?.findOne({guid: inittialClientType.guid})
         if(!guessClientType) {
             inittialClientType.project_id = projectId
-            guessClientType = await ClientType.create(inittialClientType)
+            guessClientType = await ClientType?.create(inittialClientType)
         }
 
         // checl guess role
-        let guessRole = await Role.findOne({guid: inittialRole.guid})
+        let guessRole = await Role?.findOne({guid: inittialRole.guid})
         if(!guessRole) {
             inittialRole.project_id = projectId
-            guessRole = await Role.create(inittialRole)
+            guessRole = await Role?.create(inittialRole)
         }
 
         // check guess role record permissions
-        const tables = await Table.find().lean()
+        const tables = await Table?.find().lean()
         const mapTables = {}
-        const table_slugs = tables.map(el => {
+        const table_slugs = tables?.map(el => {
             mapTables[el.slug] = el
             return el.slug
         })
-        const existRecordPermissions = await RecordPermission.find({table_slug: {$in: table_slugs}, role_id: guessRole.guid}).lean() || []
+        const existRecordPermissions = await RecordPermission?.find({table_slug: {$in: table_slugs}, role_id: guessRole.guid}).lean() || []
         const mapRecordPermissions = {}
-        existRecordPermissions.forEach(el => {
+        existRecordPermissions?.forEach(el => {
             mapRecordPermissions[el.table_slug] = el
         });
         for(let slug of table_slugs) {
@@ -55,20 +55,20 @@ module.exports = async (mongoConn, projectId) => {
         }
 
         // check guess role field permissions
-        const  fields = await Field.find().lean()
+        const  fields = await Field?.find().lean()
         let mapFields = {}
         const field_ids = fields.map(el => {
             mapFields[el.id] = el
             return el.id
         })
-        const existFieldPermissions = await FieldPermission.find({field_id: {$in: field_ids}, role_id: guessRole.guid}).lean() || []
+        const existFieldPermissions = await FieldPermission?.find({field_id: {$in: field_ids}, role_id: guessRole.guid}).lean() || []
         const mapFieldPermissions = {}
         existFieldPermissions.forEach(el => {
             mapFieldPermissions[el.field_id] = el
         });
         for(let id of field_ids) {
             if(!mapFieldPermissions[id] && mapFields[id]) {
-                await FieldPermission.create({
+                await FieldPermission?.create({
                     guid: v4(),
                     field_id: id,
                     table_slug: mapTables[mapFields[id]?.table_id],

40b80b8a99b363822788da09ba7df04a51a2c915
valiyev_muhammadjon <valiyevmuhammadjon@gmail.com>
2023-06-10 00:36:52 +0500

    test get list app

--- storage/mongo/app.js
+++ storage/mongo/app.js
@@ -84,8 +84,8 @@ let appStore = {
             let tables = []
             if (app.tables) {
                 for (const single_table of app.tables) {
-                    // table = await Table.findOne({ id: single_table.table_id})
-                    let table = await tableVersion(mongoConn, { id: single_table.table_id, deleted_at: new Date("1970-01-01T18:00:00.000+00:00") }, data.version_id, true)
+                    let table = await Table.findOne({ id: single_table.table_id})
+                    // let table = await tableVersion(mongoConn, { id: single_table.table_id, deleted_at: new Date("1970-01-01T18:00:00.000+00:00") }, data.version_id, true)
                     if (table) {
                         tables.push({
                             ...table._doc,

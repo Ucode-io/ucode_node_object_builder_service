44e1b5bf3a1c8773d6c24def8de6b9e003a0104c
valiyev_muhammadjon <valiyevmuhammadjon@gmail.com>
2023-07-05 09:12:38 +0500

    added order by

--- storage/mongo/section.js
+++ storage/mongo/section.js
@@ -301,7 +301,7 @@ let sectionStore = {
                 query,
                 null,
                 {
-                    sort: { created_at: -1 }
+                    sort: { order: -1 }
                 }
             );
             let sectionsResponse = []

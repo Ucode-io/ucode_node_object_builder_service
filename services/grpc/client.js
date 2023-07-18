40c4499976505e7c892abf2245b2eb0aa659f8b8
valiyev_muhammadjon <valiyevmuhammadjon@gmail.com>
2023-07-18 10:04:34 +0500

    check auto reconnect

--- services/grpc/client.js
+++ services/grpc/client.js
@@ -129,4 +129,4 @@ const deleteUserAuth = async (data) => {
         });
     });
 };
-module.exports = { autoConn, createUserAuth, updateUserAuth, deleteUserAuth, reConn };
+module.exports = { autoConn, createUserAuth, updateUserAuth, deleteUserAuth };

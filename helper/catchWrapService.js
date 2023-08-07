899aa4307a5ffc61ce4e5ac5aa2c1f3a652de40c
valiyev_muhammadjon <valiyevmuhammadjon@gmail.com>
2023-08-07 19:38:06 +0500

    pull dev protos

--- helper/catchWrapService.js
+++ helper/catchWrapService.js
@@ -17,7 +17,6 @@ module.exports = (namespace, fn) => {
             console.log("error getting Mongo");
             if (error.message === "db conn with given projectId does not exist") {
                 const resource = await grpcClient.reConn(config.k8s_namespace, projectId)
-                console.log("resource::", resource);
                 await projectStore.reconnect({
                     credentials: {
                         host: resource.res.credentials.host,

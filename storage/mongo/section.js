51399d8ea0e9f1cb2000111b5b8ef9fa1b5528e1
abdinazarovDV <abdinazarovgithub17@gmail.com>
2023-09-12 15:19:59 +0500

    add is_editable to fieldResponse

--- storage/mongo/section.js
+++ storage/mongo/section.js
@@ -356,6 +356,7 @@ let sectionStore = {
                                 relation_id: relation.id,
                                 relation_table_slug: data.table_slug
                             })
+                            field.is_editable = relation.is_editable
                         }
                         let tableFields = await Field.find({ table_id: data.table_id })
                         let autofillFields = []

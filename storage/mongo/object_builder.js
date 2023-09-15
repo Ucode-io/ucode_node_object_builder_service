c8414d60b68a1a93124ce999744866c2803fc6c6
valiyev_muhammadjon <valiyevmuhammadjon@gmail.com>
2023-09-15 16:54:43 +0500

    fix group by

--- storage/mongo/object_builder.js
+++ storage/mongo/object_builder.js
@@ -3007,7 +3007,7 @@ let objectBuilder = {
                 copyPipeline.push({
                     $group: {
                         _id: firstGroup,
-                        [field.slug]: `$${[field.slug]}`,
+                        [field.slug]: { $first: `$${[field.slug]}` },
                         data: {
                             $push: {
                                 ...projectColumnsWithDollorSign,

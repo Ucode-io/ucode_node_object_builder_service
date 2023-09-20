5fdc570816eb6d7fad71418e71650405b5b8d236
valiyev_muhammadjon <valiyevmuhammadjon@gmail.com>
2023-09-20 15:26:56 +0500

    fix create object

--- storage/mongo/object_builder.js
+++ storage/mongo/object_builder.js
@@ -34,14 +34,9 @@ let objectBuilder = {
         //if you will be change this function, you need to change multipleInsert function
         try {
             const mongoConn = await mongoPool.get(req.project_id)
-            const table = await tableVersion(mongoConn, { slug: req.table_slug })
+            const tableData = await tableVersion(mongoConn, { slug: req.table_slug })
             let allTableInfos = await ObjectBuilder(true, req.project_id)
             const tableInfo = allTableInfos[req.table_slug]
-            let tableData = await table.findOne(
-                {
-                    slug: req.table_slug
-                }
-            )
 
             let { payload, data, appendMany2ManyObjects } = await PrepareFunction.prepareToCreateInObjectBuilder(req, mongoConn)
             await payload.save();
@@ -94,7 +89,7 @@ let objectBuilder = {
             const object = struct.encode({ data });
             
             let customMessage = ""
-            if (table) {
+            if (tableData) {
                 const customErrMsg = await mongoConn?.models["CustomErrorMessage"]?.findOne({
                     code: 201,
                     table_id: table.id,

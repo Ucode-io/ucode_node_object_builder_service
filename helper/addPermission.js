b8cf9ae60adae96b36238a8ec184f0641772163d
abdinazarovDV <abdinazarovgithub17@gmail.com>
2023-10-12 09:59:42 +0500

    add test logs

--- helper/addPermission.js
+++ helper/addPermission.js
@@ -6,7 +6,7 @@ const config = require('../config/index')
 
 let permissionFunctions = {
     toField: async (fields, roleId, tableSlug, project_id) => {
-
+        console.log("~~~~ > ROLEID", roleId)
         let unusedFieldsSlugs = {}
 
         try {
@@ -94,6 +94,7 @@ let permissionFunctions = {
                     unusedFieldsSlugs[field.slug] = 0
                 }
             }
+            console.log(":~~~~> unused fields ", unusedFieldsSlugs)
             return {fieldsWithPermissions, unusedFieldsSlugs};
 
         } catch (err) {

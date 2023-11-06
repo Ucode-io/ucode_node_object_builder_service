0c7cdd48bdfea984c95525a7a80bb7932dabd8f2
abdinazarovDV <abdinazarovgithub17@gmail.com>
2023-11-06 13:03:58 +0500

    added node_type to autoconnecting and reconnecting resource

--- storage/mongo/project.js
+++ storage/mongo/project.js
@@ -229,7 +229,7 @@ let projectStore = {
         console.log("TEST::::::::::::::::::: 1")
         if (!config.k8s_namespace) { throw new Error("k8s_namespace is required to get project") };
 
-        let reconnect_data = await client.autoConn(config.k8s_namespace);
+        let reconnect_data = await client.autoConn(config.k8s_namespace, config.nodeType);
         console.log("TEST::::::::::::::::::: 2")
         // console.log("PROJECT-CRED >> ", reconnect_data.res.length, reconnect_data.res)
         for (let it of reconnect_data.res) {

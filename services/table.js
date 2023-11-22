0741ff086d10eace70c660694f0a799f581f9b4e
abdinazarovDV <abdinazarovgithub17@gmail.com>
2023-11-22 07:24:40 +0500

    remove sending message with kafka when table create

--- services/table.js
+++ services/table.js
@@ -64,7 +64,7 @@ const tableService = {
             event.payload = table
             event.project_id = call.request.project_id || cfg.ucodeDefaultProjectID
 
-            await sendMessageToTopic(con.TopicTableCreateV1, event)
+            // await sendMessageToTopic(con.TopicTableCreateV1, event)
 
             callback(null, {
                 id: response.id

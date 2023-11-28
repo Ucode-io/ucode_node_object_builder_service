3c3be6b36df8b0e2adf6e21792df97070424dd54
Shokhrukh <shokhrukhsafarov2003@gmail.com>
2023-11-28 07:56:02 +0500

    uuid adde

--- storage/mongo/field.js
+++ storage/mongo/field.js
@@ -120,6 +120,10 @@ let fieldStore = {
             const Table = mongoConn.models['Table']
             const Field = mongoConn.models['Field']
 
+            if (!data.id) {
+                data.id = v4()
+            }
+
             if (con.DYNAMIC_TYPES.includes(data.type) && data.autofill_field && data.autofill_table) {
                 // let autoFillTable = await Table.findOne({
                 //     slug: data.autofill_table,

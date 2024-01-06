334fc8935ce998b62b2438ac864bd0f0fb09e070
Shokhrukh <shokhrukhsafarov2003@gmail.com>
2024-01-06 14:56:34 +0500

    changed record_permission

--- helper/new_field.js
+++ helper/new_field.js
@@ -2,7 +2,7 @@ module.exports = async function (data) {
     try {
         const Menu = data.mongoDBConn.models['record_permission'];
 
-        console.log("Adding add_field and pdf_action fields to menu with langauge_btn");
+        console.log("Updating add_field and pdf_action fields based on the presence of language_btn");
 
         Menu.updateMany(
             { language_btn: { $exists: true } },
@@ -17,7 +17,19 @@ module.exports = async function (data) {
             }
         );
 
-        console.log("add_field and pdf_action fields added to menu with langauge_btn");
+        Menu.updateMany(
+            { language_btn: { $exists: false } },
+            { $unset: { add_field: '', pdf_action: '' } },
+            (err, result) => {
+                if (err) {
+                    console.error('Error:', err);
+                    return;
+                }
+
+                console.log(`${result.modifiedCount} documents updated successfully.`);
+            }
+        );
+
     } catch (error) {
         console.log("Error in yourFileName.js", error);
     }

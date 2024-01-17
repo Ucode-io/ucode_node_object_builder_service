4b94de83b92cd80acd93cfc9a0a05fb3130fa01c
abdinazarovDV <abdinazarovgithub17@gmail.com>
2024-01-17 10:31:59 +0500

    restart

--- storage/mongo/project.js
+++ storage/mongo/project.js
@@ -242,7 +242,7 @@ let projectStore = {
         if (!config.k8s_namespace) { throw new Error("k8s_namespace is required to get project") };
 
         let reconnect_data = await client.autoConn(config.k8s_namespace, config.nodeType);
-        // console.log("TEST::::::::::::::::::: 2")
+    
         // console.log("PROJECT-CRED >> ", reconnect_data.res.length, reconnect_data.res)
         for (let it of reconnect_data.res) {
             // console.log("credentials:::", it.resource_type)

8e9d629e79ed79093e1275fe97f5ac08acad040e
Shokhrukh <shokhrukhsafarov2003@gmail.com>
2024-01-17 13:09:36 +0500

    change menu proto

--- index_dev.js
+++ index_dev.js
@@ -28,7 +28,7 @@ const logger = require("./config/logger");
         logger.info(`autoconnectinag to resources`);
         await projectStorage.reconnect({
             // project_id: "b53d9192-d46a-484e-8ecd-d9344df18e4a",
-            project_id: "dc3b8f74-aa46-4101-b255-d6b82ac0db2d",
+            project_id: "dc3b8f74-aa46-4101-b255-d6b82ac0db2d   ",
             credentials: {
                 host: "65.109.239.69",
                 port: 30027,

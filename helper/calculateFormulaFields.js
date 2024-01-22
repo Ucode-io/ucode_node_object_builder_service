cc73f79c17814bfe83996d146dbadc55b7dc12fc
Shokhrukh <shokhrukhsafarov2003@gmail.com>
2024-01-22 09:01:21 +0500

    fixed calculate formula bug

--- helper/calculateFormulaFields.js
+++ helper/calculateFormulaFields.js
@@ -17,7 +17,7 @@ let formulaFunction = {
                 value = JSON.stringify(value).toUpperCase()
             }
             if (typeof value === "string") {
-                value = `${value}`
+                value = `'${value}'`
             }
             if (typeof value === "object") {
                 value = `'${value[0]}'`

59c10d227d764958d49fd044a22f1e8355835cfa
Shokhrukh <shokhrukhsafarov2003@gmail.com>
2024-02-20 14:25:52 +0500

    restart

--- index_dev.js
+++ index_dev.js
@@ -18,7 +18,7 @@ const logger = require("./config/logger");
 
     process.addListener("uncaughtException", (e) => {
         console.error("Custom Unhandled Exception", e);
-    });
+    }); 
 
     process.addListener("unhandledRejection", (e) => {
         console.error("Custom Unhandled Rejection", e);

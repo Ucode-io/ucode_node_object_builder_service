ab45f12ca2f24b674d089316ee28739a0851ae0f
Shokhrukh <shokhrukhsafarov2003@gmail.com>
2024-03-13 20:12:13 +0500

    removed creds from config

--- config/index.js
+++ config/index.js
@@ -1,34 +1,34 @@
 const config = {
     environment: getConf("NODE_ENV", "dev"),
 
-    mongoHost: getConf("MONGO_HOST", "161.35.26.178"),
-    mongoPort: getConf("MONGO_PORT", "27017"),
-    mongoUser: getConf("MONGO_USER", "object_builder_service"),
-    mongoPassword: getConf("MONGO_PASSWORD", "eeNgoot2"),
-    mongoDatabase: getConf("MONGO_DATABASE", "object_builder_service"),
+    mongoHost: getConf("MONGO_HOST", ""),
+    mongoPort: getConf("MONGO_PORT", ""),
+    mongoUser: getConf("MONGO_USER", ""),
+    mongoPassword: getConf("MONGO_PASSWORD", ""),
+    mongoDatabase: getConf("MONGO_DATABASE", ""),
 
-    RPCPort: getConf("RPC_PORT", 9102),
+    RPCPort: getConf("RPC_PORT", 0),
 
-    kafkaHost: getConf("KAFKA_HOST", "localhost"),
-    kafkaPort: getConf("KAFKA_PORT", "9092"),
-    kafkaClientId: getConf("KAFKA_CLIENT_ID", "medion_node_object_builder_service"),
+    kafkaHost: getConf("KAFKA_HOST", ""),
+    kafkaPort: getConf("KAFKA_PORT", ""),
+    kafkaClientId: getConf("KAFKA_CLIENT_ID", ""),
 
     limit: getConf("DEFAULT_LIMIT", 10),
     page: getConf("DEFAULT_PAGE", 1),
 
-    minioAccessKeyID: getConf("MINIO_ACCESS_KEY", "minio-access-key"),
-    minioSecretAccessKey: getConf("MINIO_SECRET_KEY", "minio-secret-key"),
-    minioEndpoint: getConf("MINIO_ENDPOINT", "minio-endpoint"),
-    minioPort: getConf("MINIO_PORT", 9001),
+    minioAccessKeyID: getConf("MINIO_ACCESS_KEY", ""),
+    minioSecretAccessKey: getConf("MINIO_SECRET_KEY", ""),
+    minioEndpoint: getConf("MINIO_ENDPOINT", ""),
+    minioPort: getConf("MINIO_PORT", 0),
     minioSSL: getConf("MINIO_SSL", true),
 
     ucodeDefaultProjectID: "ucode_default_project_id",
     k8s_namespace: "cp-region-type-id",
 
-    companyServiceHost: getConf("COMPANY_SERVICE_HOST", "localhost"),
-    companyServicePort: getConf("COMPANY_GRPC_PORT", ":8092"),
-    authServiceHost: getConf("AUTH_SERVICE_HOST", "localhost"),
-    authServicePort: getConf("AUTH_GRPC_PORT", ":9103"),
+    companyServiceHost: getConf("COMPANY_SERVICE_HOST", ""),
+    companyServicePort: getConf("COMPANY_GRPC_PORT", ""),
+    authServiceHost: getConf("AUTH_SERVICE_HOST", ""),
+    authServicePort: getConf("AUTH_GRPC_PORT", ""),
 
     nodeType: getConf("NODE_TYPE", "LOW")
 };

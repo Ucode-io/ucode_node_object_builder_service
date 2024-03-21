3bab7fea9027d23f2c8353c9dcd3f5abd2217ce7
o.muzropov <otabek.muzropov2001@gmail.com>
2024-03-21 18:31:03 +0500

    client_type_id, role_id and user_id are added to register project

--- index_dev.js
+++ index_dev.js
@@ -28,13 +28,13 @@ const logger = require("./config/logger");
         logger.info(`autoconnectinag to resources`);
         await projectStorage.reconnect({
             // project_id: "b53d9192-d46a-484e-8ecd-d9344df18e4a",
-            project_id: "e7650423-b575-4033-9dbe-b28f3526ba13",
+            project_id: "4219c6a5-8754-47f7-aa6b-379919ed0fae",
             credentials: {
                 host: "65.109.239.69",
                 port: 30027,
-                database: "marvel_cb8f05e05dd3436d93d0cc31ed6874c2_p_obj_build_svcs",
-                username: "marvel_cb8f05e05dd3436d93d0cc31ed6874c2_p_obj_build_svcs",
-                password: "XXKIzpW6oG"
+                database: "balenciaga_d94b7c0e3f6446e7a362345c9fe6800d_p_obj_build_svcs",
+                username: "balenciaga_d94b7c0e3f6446e7a362345c9fe6800d_p_obj_build_svcs",
+                password: "Dov4ocrqq0"
             }
         })
 
@@ -42,7 +42,7 @@ const logger = require("./config/logger");
         //mongodb://ett_8fffdb8cbff54645a40dbae0a272f6e7_p_obj_build_svcs:ugdvopAnMJ@65.109.239.69:30027/ett_8fffdb8cbff54645a40dbae0a272f6e7_p_obj_build_svcs
         //mongodb://ett_8fffdb8cbff54645a40dbae0a272f6e7_s_obj_build_svcs:AvZtXGmSFi@65.109.239.69:30027/ett_8fffdb8cbff54645a40dbae0a272f6e7_s_obj_build_svcs
         //mongodb://uzcard_1cc4c7e54cc64415b074f331c6a13cc1_p_obj_build_svcs:vBPqHBE2gm@142.93.164.37:27017/uzcard_1cc4c7e54cc64415b074f331c6a13cc1_p_obj_build_svcs
-
+        //mongodb://balenciaga_d94b7c0e3f6446e7a362345c9fe6800d_p_obj_build_svcs:Dov4ocrqq0@65.109.239.69:30027/balenciaga_d94b7c0e3f6446e7a362345c9fe6800d_p_obj_build_svcs
         //mongodb://swift_7214baf774da4fd2a1166477a9528c83_p_obj_build_svcs:vwdG8B7gzO@142.93.164.37:27017/swift_7214baf774da4fd2a1166477a9528c83_p_obj_build_svcs
         //mongodb://dev_b7a9b7317ba04a97a1ecebc6da74f0af_p_obj_build_svcs:QoRidVg9iL@65.109.239.69:30027/dev_b7a9b7317ba04a97a1ecebc6da74f0af_p_obj_build_svcs
         //mongodb://rizo-company_rizo-company_object_builder_service:uEzqO8YsIt@142.93.164.37:27017/rizo-company_rizo-company_object_builder_service

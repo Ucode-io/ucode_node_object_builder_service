c170c81164201deab4ee0dfc5b6337027ebe21a4
o.muzropov <otabek.muzropov2001@gmail.com>
2024-03-26 13:51:45 +0500

    primary key columns

--- helper/generator.js
+++ helper/generator.js
@@ -28,4 +28,15 @@ function generateRandomNumberWithOutDash(prefix, digitNumber) {
     return result
 }
 
-module.exports = {generateBarcode, generateRandomNumber, generateRandomNumberWithOutDash}
+function generateRandomString(length) {
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let result = '';
+    for (let i = 0; i < length; i++) {
+        const randomIndex = Math.floor(Math.random() * characters.length);
+        result += characters[randomIndex];
+    }
+    return result;
+}
+
+
+module.exports = {generateBarcode, generateRandomNumber, generateRandomNumberWithOutDash, generateRandomString}

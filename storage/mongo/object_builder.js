9845e5ffc8a2b45dd2296b7e0fef0c44efa729a9
s.erkinboyev <samandargobackend@gmail.com>
2024-06-03 13:22:19 +0500

    table detailes relation fields edited

--- storage/mongo/object_builder.js
+++ storage/mongo/object_builder.js
@@ -3755,6 +3755,7 @@ let objectBuilder = {
                                       if (newmapCount[newField.id] > 1) {
                                         let toaddnum = baseSlug.split("_data");
                                         newField.path_slug = `${toaddnum[0]}_${newmapCount[newField.id]}_data.${parts[1]}`;
+                                        newField.label = newField.label + " " + newmapCount[newField.id]
                                       } else if (newmapCount[newField.id] == 0) {
                                         let toaddnum = baseSlug.split("_data");
                                         newField.path_slug = `${toaddnum[0]}_data.${parts[1]}`;

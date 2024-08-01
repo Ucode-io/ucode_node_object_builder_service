d14b868029754cb8c6a0d8a5986052ffe4aeeb87
Shokhrukh Safarov <safarovshohruh2803@gmail.com>
2024-08-01 07:21:50 +0500

    update record permission bug fixed

--- helper/new_field.js
+++ helper/new_field.js
@@ -3,15 +3,15 @@ module.exports = async function (data) {
         const Menu = data.mongoDBConn.models['record_permission'];
         const Field = data.mongoDBConn.models['Field'];
 
-        Menu.updateMany(
-            { language_btn: { $exists: true } },
-            { $set: { add_field: 'Yes', pdf_action: 'Yes', add_filter: 'Yes', field_filter: 'Yes', fix_column: 'Yes', tab_group: 'Yes', columns: 'Yes', group: 'Yes', excel_menu: 'Yes', search_button: 'Yes' } },
-            (err, result) => {
-                if (err) {
-                    return;
-                }
-            }
-        );
+        // Menu.updateMany(
+        //     { language_btn: { $exists: true } },
+        //     { $set: { add_field: 'Yes', pdf_action: 'Yes', add_filter: 'Yes', field_filter: 'Yes', fix_column: 'Yes', tab_group: 'Yes', columns: 'Yes', group: 'Yes', excel_menu: 'Yes', search_button: 'Yes' } },
+        //     (err, result) => {
+        //         if (err) {
+        //             return;
+        //         }
+        //     }
+        // );
 
         Menu.updateMany(
             { language_btn: { $exists: false } },

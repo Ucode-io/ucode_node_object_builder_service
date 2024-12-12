2315b3b921c3a461edd76d5750a26d8acf6c6e9a
OtabekMuzropov <otabek.muzropov2001@gmail.com>
2024-12-12 08:45:20 +0500

    check with user id auth when update login table item

--- storage/mongo/object_builder.js
+++ storage/mongo/object_builder.js
@@ -251,7 +251,7 @@ let objectBuilder = {
                                             table_slug: tableModel.slug
                                         });
 
-                                        if (loginTable) {
+                                        if (loginTable && response['user_id_auth'] !== '') {
                                             let updateUserRequest = {
                                                 env_id: req.env_id,
                                                 phone: data[authInfo['phone']],
@@ -301,7 +301,7 @@ let objectBuilder = {
                                             table_slug: tableModel.slug
                                         });
 
-                                        if (loginTable) {
+                                        if (loginTable && response['user_id_auth'] !== '') {
                                             let updateUserRequest = {
                                                 env_id: req.env_id,
                                                 phone: data[authInfo['phone']],
@@ -347,7 +347,7 @@ let objectBuilder = {
                                         table_slug: tableModel.slug
                                     });
 
-                                    if (loginTable) {
+                                    if (loginTable && response['user_id_auth'] !== '') {
                                         let updateUserRequest = {
                                             env_id: req.env_id,
                                             phone: data[authInfo['phone']],

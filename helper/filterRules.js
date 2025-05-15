4e992de5cbfbb87f7307f716b1b17afaf90de8e8
webslingerr <shokhrukhsafarov2003@gmail.com>
2025-05-15 14:02:11 +0500

    filter rules changed

--- helper/filterRules.js
+++ helper/filterRules.js
@@ -1,33 +1,33 @@
 const QUERY_KEYS_MAP = {
-    '_in': '$in',
-    '_eq': '$eq',
-    '_ne': '$ne',
-    '_nin': '$nin',
-    '_lt': '$lt',
-    '_lte': '$lte',
-    '_gt': '$gt',
-    '_gte': '$gte',
-    '_null': 'null',
-    '_nnull': { $ne: null },
-    '_contains': '$regex',                                                                  //Contains the substring
-    '_icontains': "icontains",                                                              //Contains the case-insensitive substring
-    '_ncontains': "ncontains",                                                              //Doesn't contain the substring
-    '_starts_with': 'starts_with',                                                          //Starts with
-    '_istarts_with': 'istarts_with',                                                        //Starts with, case-insensitive
-    '_nstarts_with': 'nstarts_with',                                                        //Doesn't start with
-    '_nistarts_with': 'nistarts_with',	                                                    //Doesn't start with, case-insensitive
-    '_ends_with': 'ends_with',                                                              //Ends with
-    '_iends_with': 'iends_with',                                                            //Ends with, case-insensitive
-    '_nends_with': 'nends_with',	                                                        //Doesn't end with
-    '_niends_with': 'niends_with',	                                                        //Doesn't end with, case-insensitive
-    '_between': { '$gte': 'first_value_of_array', '$lte': 'second_value_of_array' },	    //Is between two values (inclusive)
-    '_nbetween': { '$lte': 'first_value_of_array', '$gte': 'second_value_of_array' },       //Is not between two values (inclusive)
-    '_empty': 'null',                                                                       //Is empty (null or falsy)
-    '_nempty': { $ne: null }, 	                                                            //Is not empty (null or falsy)
-    '_intersects': 'intersects',                                                            //Value intersects a given point
-    '_nintersects': 'nintersects',                                                          //Value does not intersect a given point
-    '_intersects_bbox': 'intersects_bbox',                                                  //Value is in a bounding box
-    '_nintersects_bbox': 'nintersects_bbox',                                                //Value is not in a bounding box
+    '$in': '$in',
+    '$eq': '$eq',
+    '$ne': '$ne',
+    '$nin': '$nin',
+    '$lt': '$lt',
+    '$lte': '$lte',
+    '$gt': '$gt',
+    '$gte': '$gte',
+    '$null': 'null',
+    '$nnull': { $ne: null },
+    '$contains': '$regex',                                                                  //Contains the substring
+    '$icontains': "icontains",                                                              //Contains the case-insensitive substring
+    '$ncontains': "ncontains",                                                              //Doesn't contain the substring
+    '$starts_with': 'starts_with',                                                          //Starts with
+    '$istarts_with': 'istarts_with',                                                        //Starts with, case-insensitive
+    '$nstarts_with': 'nstarts_with',                                                        //Doesn't start with
+    '$nistarts_with': 'nistarts_with',	                                                    //Doesn't start with, case-insensitive
+    '$ends_with': 'ends_with',                                                              //Ends with
+    '$iends_with': 'iends_with',                                                            //Ends with, case-insensitive
+    '$nends_with': 'nends_with',	                                                        //Doesn't end with
+    '$niends_with': 'niends_with',	                                                        //Doesn't end with, case-insensitive
+    '$between': { '$gte': 'first_value_of_array', '$lte': 'second_value_of_array' },	    //Is between two values (inclusive)
+    '$nbetween': { '$lte': 'first_value_of_array', '$gte': 'second_value_of_array' },       //Is not between two values (inclusive)
+    '$empty': 'null',                                                                       //Is empty (null or falsy)
+    '$nempty': { $ne: null }, 	                                                            //Is not empty (null or falsy)
+    '$intersects': 'intersects',                                                            //Value intersects a given point
+    '$nintersects': 'nintersects',                                                          //Value does not intersect a given point
+    '$intersects_bbox': 'intersects_bbox',                                                  //Value is in a bounding box
+    '$nintersects_bbox': 'nintersects_bbox',                                                //Value is not in a bounding box
 }
 
 
@@ -38,64 +38,64 @@ module.exports = (key, value) => {
     let query = ""
     if (QUERY_KEYS_MAP[key]) {
         switch (key) {
-            case '_null':
-            case '_empty':
+            case '$null':
+            case '$empty':
                 query = null
                 break;
-            case '_nnull':
-            case '_nempty':
+            case '$nnull':
+            case '$nempty':
                 query = { $ne: null }
                 break;
-            case '_in':
-            case '_nin':
+            case '$in':
+            case '$nin':
                 if (!Array.isArray(value)) throw new TypeError('expected array, got ' + value)
                 query = { [QUERY_KEYS_MAP[key]]: value }
                 break;
-            case '_icontains':
+            case '$icontains':
                 query = { $regex: value, $options: 'i' }
                 break;
-            case '_ncontains':
+            case '$ncontains':
                 query = { $not: { $regex: value } }
                 break;
-            case '_starts_with':
+            case '$starts_with':
                 query = { $regex: `^${value}` }
                 break;
-            case '_istarts_with':
+            case '$istarts_with':
                 query = { $regex: `^${value}`, $options: 'i' }
                 break;
-            case '_nstarts_with':
+            case '$nstarts_with':
                 query = { $not: { $regex: `^${value}` } }
                 break;
-            case '_nistarts_with':
+            case '$nistarts_with':
                 query = { $not: { $regex: `^${value}`, $options: 'i' } }
                 break;
-            case '_ends_with':
+            case '$ends_with':
                 query = { $regex: `${value}$` }
                 break;
-            case '_iends_with':
+            case '$iends_with':
                 query = { $regex: `${value}$`, $options: 'i' }
                 break;
-            case '_nends_with':
+            case '$nends_with':
                 query = { $not: { $regex: `${value}$` } }
                 break;
-            case '_niends_with':
+            case '$niends_with':
                 query = { $not: { $regex: `${value}$`, $options: 'i' } }
                 break;
-            case '_between':
+            case '$between':
                 if (!Array.isArray(value)) throw new TypeError('expected array, got ' + value)
                 query = {
                     $gte: value[0],
                     $lte: value[1]
                 }
                 break;
-            case '_nbetween':
+            case '$nbetween':
                 if (!Array.isArray(value)) throw new TypeError('expected array, got ' + value)
                 query = {
                     $gte: value[1],
                     $lte: value[0]
                 }
                 break;
-            case '_intersects':
+            case '$intersects':
                 let geoIntersect = {
                     type: 'Polygon',
                     coordinates: [
@@ -110,7 +110,7 @@ module.exports = (key, value) => {
                 };
                 query = { $geoIntersects: { $geometry: geoIntersect } }
                 break;
-            case '_nintersects':
+            case '$nintersects':
                 let notGeoIntersect = {
                     type: 'Polygon',
                     coordinates: [
@@ -125,7 +125,7 @@ module.exports = (key, value) => {
                 };
                 query = { $not: { $geoIntersects: { $geometry: notGeoIntersect } } }
                 break;
-            case '_intersects_bbox':
+            case '$intersects_bbox':
                 let bbox = {
                     type: 'Polygon',
                     coordinates: [
@@ -140,7 +140,7 @@ module.exports = (key, value) => {
                 };
                 query = { $geoWithin: { $geometry: bbox } }
                 break;
-            case '_nintersects_bbox':
+            case '$nintersects_bbox':
                 let notBbox = {
                     type: 'Polygon',
                     coordinates: [
